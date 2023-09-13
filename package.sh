eea9a14efac817144a182278518f7854571ecb10
Ben Parrish <ben.parrish@skipify.com>
2023-09-13 13:27:12 -0400

    Packaging

---
+++ package.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+zip -x "*.zip" -x "dist/" -x "dist/**" -x ".git/" -x "/.git/**" -x "package.sh" -x "README.md" -9 -r dist/sunrunner.love .
+echo dist/love.exe dist/sunrunner.love dist/sunrunner.exe
+rm dist/sunrunner.love
+cd dist
+zip -x "love.exe" -9 -r ../sunrunner-$(date +%Y%m%d-%H%M%S).zip .
+cd ..
